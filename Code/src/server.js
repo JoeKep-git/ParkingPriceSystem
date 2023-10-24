d25cac1580f992cab39bbefe26a209385d5982f9
JoeK <joeykeputabusiness@gmail.com>
2023-10-24 13:51:35 +0100

    added get method that retrieves all the data from the urls for fuel prices and prints a JSON in front end currently

--- Code/src/server.js
+++ Code/src/server.js
@@ -3,14 +3,16 @@ const fs = require('fs');
 const express = require("express");
 const app = express();
 const env = require('dotenv').config();
+const axios = require('axios');
+const request = require('request-promise');
+const { resolve } = require('path');
+const zlib = require('zlib');
 
 //HTTPS Certificates
 const options = {
     key: fs.readFileSync(__dirname+'/Certificates/server.key'),
     cert: fs.readFileSync(__dirname+'/Certificates/server.crt')
 }
- 
-
 
 //SERVER PORT
 const PORT = process.env.PORT || 8000;
@@ -43,12 +45,14 @@ app.get('/', (req, res) => {
     Shell	https://www.shell.co.uk/fuel-prices-data.html
     Tesco	https://www.tesco.com/fuel_prices/fuel_prices_data.json
  */
+
+//URLS available on government website for fuel prices in uk
 const urls = [
     'https://applegreenstores.com/fuel-prices/data.json',
     'https://fuelprices.asconagroup.co.uk/newfuel.json',
     'https://storelocator.asda.com/fuel_prices_data.json',
     'https://www.bp.com/en_gb/united-kingdom/home/fuelprices/fuel_prices_data.json',
-   	'https://www.esso.co.uk/-/media/Project/WEP/Esso/Esso-Retail-UK/roadfuelpricingscheme',
+    'https://www.esso.co.uk/-/media/Project/WEP/Esso/Esso-Retail-UK/roadfuelpricingscheme',
     'https://images.morrisons.com/petrol-prices/petrol.json',
     'https://fuel.motorfuelgroup.com/fuel_prices_data.json',
     'https://www.rontec-servicestations.co.uk/fuel-prices/data/fuel_prices_data.json',
@@ -57,9 +61,72 @@ const urls = [
     'https://www.shell.co.uk/fuel-prices-data.html',
     'https://www.tesco.com/fuel_prices/fuel_prices_data.json'
 ];
-//TODO: read json links and display them on the map
 
+//Function to get data from url
+function getData(url, timeout) {
+    return new Promise((resolve, reject) => {
+        const options = {
+            headers: {
+                'Accept-Encoding': 'gzip'
+            }
+        };
 
+        //HTTPS get request
+        //using gzip for compression
+        const req = https.get(url, options, (res) => {
+            let data = '';
+            
+            const encoding = res.headers['content-encoding'];
+            const stream = encoding === 'gzip' ? res.pipe(zlib.createGunzip()) : res;
+
+            stream.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            stream.on('end', () => {
+                resolve(data);
+            });
+        }).on('error', (err) => {
+            reject(err);
+        });
+
+        req.on('timeout', () => {
+            reject(new Error('Request timed out for url: ' + url));
+        });
+
+        req.setTimeout(timeout);
+    });
+}
+
+//Timeout for get requests
+const timeout = 5000; // 5 seconds
+
+//GETTING THE DATA FROM THE URLS
+app.get('/data', async (req, res) => {
+    let responseData = [];
+
+    for (const url of urls) {
+        //throwing error in getData and catching it here
+        try {
+            //prints each url i am getting data from
+            console.log(`Fetching data from ${url}`);
+
+            //getting data from url
+            const data = await getData(url, timeout);
+            //if data is not empty then parse it and push it to responseData
+            if (data.trim() !== "") {
+                const parsedData = JSON.parse(data);
+                parsedData.stations.forEach((station) => {
+                    responseData.push(station);
+                });
+            }
+        } catch (err) {
+            console.error(`Error fetching data from ${url}:`, err);
+        }
+    }
+
+    res.json(responseData);
+});
 
 //create HTTPS server
 const server = https.createServer(options, app).listen(PORT, () => {
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ParkingPriceSystem
 This is for my project where I make a system to find the nearest cheapest petrol stations
+
+Run the `npm ci` to install the packages when in the code directory
