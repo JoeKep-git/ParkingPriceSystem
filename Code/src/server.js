86181d6b8812f1b82a18924d4c43a9b80f63469e
JoeK <joeykeputabusiness@gmail.com>
2023-10-24 11:26:35 +0100

    moved comment for urls

--- Code/src/server.js
+++ Code/src/server.js
@@ -29,6 +29,20 @@ app.get('/', (req, res) => {
 });
 
 //TODO: try and get the bing api to work on backend and then send the data to the frontend
+/**
+ *  Applegreen UK	https://applegreenstores.com/fuel-prices/data.json
+    Ascona Group	https://fuelprices.asconagroup.co.uk/newfuel.json
+    Asda	https://storelocator.asda.com/fuel_prices_data.json
+    bp	https://www.bp.com/en_gb/united-kingdom/home/fuelprices/fuel_prices_data.json
+    Esso Tesco Alliance	https://www.esso.co.uk/-/media/Project/WEP/Esso/Esso-Retail-UK/roadfuelpricingscheme
+    Morrisons	https://images.morrisons.com/petrol-prices/petrol.json
+    Motor Fuel Group	https://fuel.motorfuelgroup.com/fuel_prices_data.json
+    Rontec	https://www.rontec-servicestations.co.uk/fuel-prices/data/fuel_prices_data.json
+    Sainsbury’s	https://api.sainsburys.co.uk/v1/exports/latest/fuel_prices_data.json
+    SGN	https://www.sgnretail.uk/files/data/SGN_daily_fuel_prices.json
+    Shell	https://www.shell.co.uk/fuel-prices-data.html
+    Tesco	https://www.tesco.com/fuel_prices/fuel_prices_data.json
+ */
 const urls = [
     'https://applegreenstores.com/fuel-prices/data.json',
     'https://fuelprices.asconagroup.co.uk/newfuel.json',
@@ -45,20 +59,7 @@ const urls = [
 ];
 //TODO: read json links and display them on the map
 
-/**
- *  Applegreen UK	https://applegreenstores.com/fuel-prices/data.json
-    Ascona Group	https://fuelprices.asconagroup.co.uk/newfuel.json
-    Asda	https://storelocator.asda.com/fuel_prices_data.json
-    bp	https://www.bp.com/en_gb/united-kingdom/home/fuelprices/fuel_prices_data.json
-    Esso Tesco Alliance	https://www.esso.co.uk/-/media/Project/WEP/Esso/Esso-Retail-UK/roadfuelpricingscheme
-    Morrisons	https://images.morrisons.com/petrol-prices/petrol.json
-    Motor Fuel Group	https://fuel.motorfuelgroup.com/fuel_prices_data.json
-    Rontec	https://www.rontec-servicestations.co.uk/fuel-prices/data/fuel_prices_data.json
-    Sainsbury’s	https://api.sainsburys.co.uk/v1/exports/latest/fuel_prices_data.json
-    SGN	https://www.sgnretail.uk/files/data/SGN_daily_fuel_prices.json
-    Shell	https://www.shell.co.uk/fuel-prices-data.html
-    Tesco	https://www.tesco.com/fuel_prices/fuel_prices_data.json
- */
+
 
 //create HTTPS server
 const server = https.createServer(options, app).listen(PORT, () => {
