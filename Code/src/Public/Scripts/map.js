0522a744847e504d271b1e3e2f2a9cc3a6d4fc5d
JoeK <joeykeputabusiness@gmail.com>
2023-10-25 14:51:47 +0100

    pins are now where petrol stations are. Tesco api still not working

--- Code/src/Public/Scripts/map.js
+++ Code/src/Public/Scripts/map.js
@@ -1,21 +1,45 @@
 let map;
 
 function getMap() {
-    map = new Microsoft.Maps.Map('#map', {
-        center: new Microsoft.Maps.Location(51.5074, 0.1272),
-        zoom: 10
+    //Initialize a map instance.
+    var map = new Microsoft.Maps.Map('#map');
+    var infoBox = new Microsoft.Maps.Infobox(map.getCenter(), {
+        visible: false
     });
 
-    //Add a clickable spot
-    const pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), {
-        title: 'Clickable Spot',
-        subTitle: 'This spot is clickable',
-        color: 'red'
-    });
+    map.entities.push(infoBox);
 
-    Microsoft.Maps.Events.addHandler(pushpin, 'click', function () {
-        alert('Clicked pushpin');
-    });
+    //GET request for my /data route
+    fetch('/data').then(response => response.json()).then(data => {
+        data.forEach(station => {
+            console.log(station);
+            console.log(station.location.latitude);
+            var location = new Microsoft.Maps.Location(station.location.latitude,station.location.longitude);
+            var pin = new Microsoft.Maps.Pushpin(location);
 
-    map.entities.push(pushpin);
+            //Click event to pushpin
+            Microsoft.Maps.Events.addHandler(pin, 'click', function () {
+                infoBox.setOptions({
+                    location: location,
+                    title: station.name,
+                    description: station.address
+                });
+                infoBox.setMap(map)
+            });
+            map.entities.push(pin);
+        });
+    }).catch(err => console.error('Error at: ', err));
+
+    // //Add a clickable spot
+    // const pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), {
+    //     title: 'Clickable Spot',
+    //     subTitle: 'This spot is clickable',
+    //     color: 'red'
+    // });
+
+    // Microsoft.Maps.Events.addHandler(pushpin, 'click', function () {
+    //     alert('Clicked pushpin');
+    // });
+
+    // map.entities.push(pushpin);
 }
